f5a3da83fa3213478c6c11c810dc1b81ed3acd07
RubyBe <sidbenavente@hotmail.com>
2016-07-28 19:45:49 -0700

    add failing test for Spec #1, StylistCreated

---
+++ Objects/Stylist.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Salon
+{
+  public class Stylist
+  {
+    // Properties
+    private int _id;
+    private string _name;
+    private string _specialty;
+
+    // Constructors
+    // A constructor that takes two paramaters and auto-assigns id
+    public Stylist(string name, string specialty, int id=0)
+    {
+      // TODO
+    }
+
+    // Getters and Setters
+    public string GetName()
+    {
+      return _name;
+    }
+
+    // Other methods
+  }
+}
--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -1,53 +1,61 @@
+using System;
 using Xunit;
 
 namespace Salon
 {
-  public class StylistTest()
+  public class StylistTest
   {
     [Fact]
-    public method New_CreatesNewStylist_StylistCreated()
+    public void New_CreatesNewStylist_StylistCreated()
+    {
+      // Arrange
+      string name = "Doc Gonzo";
+      string speciality = "Fur";
+      // Act
+      Stylist testStylist = new Stylist(name, speciality);
+      string testName = testStylist.GetName();
+      // Assert
+      Console.WriteLine(testStylist);
+      Assert.Equal(name, testName);
+
+    }
+    [Fact]
+    public void Saves_SavesNewStylist_StylistSaved()
     {
       // Arrange
       // Act
       // Assert
     }
     [Fact]
-    public method Saves_SavesNewStylist_StylistSaved()
+    public void GetAll_GetsAllStylists_AllStylistsReturned()
     {
       // Arrange
       // Act
       // Assert
     }
     [Fact]
-    public method GetAll_GetsAllStylists_AllStylistsReturned()
+    public void DeleteAll_DeletesAllStylists_AllStylistsDeleted()
     {
       // Arrange
       // Act
       // Assert
     }
     [Fact]
-    public method DeleteAll_DeletesAllStylists_AllStylistsDeleted()
+    public void Find_FindsAStylist_StylistFound()
     {
       // Arrange
       // Act
       // Assert
     }
     [Fact]
-    public method Find_FindsAStylist_StylistFound()
+    public void Update_UpdatesAStylist_StylistUpdated()
     {
       // Arrange
       // Act
       // Assert
     }
     [Fact]
-    public method Update_UpdatesAStylist_StylistUpdated()
-    {
-      // Arrange
-      // Act
-      // Assert
-    }
-    [Fact]
-    public method Delete_DeletesASingleStylist_SingleStylistDeleted()
+    public void Delete_DeletesASingleStylist_SingleStylistDeleted()
     {
       // Arrange
       // Act
