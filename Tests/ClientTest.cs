15c37a84830b60128089afea19f4e92c04042c9f
RubyBe <sidbenavente@hotmail.com>
2016-07-29 10:40:14 -0700

    add specs/test signatures for the Client class

---
+++ Tests/ClientTest.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+
+namespace Salon
+{
+  public class ClientTest
+  {
+    [Fact]
+    public void New_CreatesNewClient_ClientCreated()
+    {
+    }
+    [Fact]
+    public void Saves_SavesNewClient_ClientSaved()
+    {
+    }
+    [Fact]
+    public void GetAll_GetsAllClients_AllClientsReturned()
+    {
+    }
+    [Fact]
+    public void DeleteAll_DeletesAllClients_AllClientsDeleted()
+    {
+    }
+    [Fact]
+    public void FindById_FindsAClientById_ClientFoundById()
+    {
+    }
+    [Fact]
+    public void Update_UpdatesAClient_ClientUpdated()
+    {
+    }
+    [Fact]
+    public void Delete_DeletesASingleClient_SingleClientDeleted()
+    {
+    }
+  }
+}
