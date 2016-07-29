23e2fb86b61600e6046c0c4f6e43679150dba62a
RubyBe <sidbenavente@hotmail.com>
2016-07-28 19:02:13 -0700

    add specs/test signatures for the Stylist class

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,57 @@
+using Xunit;
+
+namespace Salon
+{
+  public class StylistTest()
+  {
+    [Fact]
+    public method New_CreatesNewStylist_StylistCreated()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method Saves_SavesNewStylist_StylistSaved()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method GetAll_GetsAllStylists_AllStylistsReturned()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method DeleteAll_DeletesAllStylists_AllStylistsDeleted()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method Find_FindsAStylist_StylistFound()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method Update_UpdatesAStylist_StylistUpdated()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+    [Fact]
+    public method Delete_DeletesASingleStylist_SingleStylistDeleted()
+    {
+      // Arrange
+      // Act
+      // Assert
+    }
+  }
+}
