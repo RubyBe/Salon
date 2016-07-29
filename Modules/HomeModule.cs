29e04abcb126979e4025925e1f843523ffcd281f
RubyBe <sidbenavente@hotmail.com>
2016-07-29 10:32:49 -0700

    add Nancy routing and view placeholders for stylist display/add functionality

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,14 @@ namespace Salon
         List<Stylist> allStylists = Stylist.GetAll();
         return View["index.cshtml", allStylists];
       };
+      Get["/stylists/new"] = _ =>
+      {
+        return View["stylists_new.cshtml"];
+      };
+      Post["/stylists/new"] = _ =>
+      {
+        return View["success.cshtml"];
+      };
     }
   }
 }
